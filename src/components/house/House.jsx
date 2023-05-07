6a84de1bb28f762cef7a3e92e8e07de8ce166fa4
SFERAS <rthyssen73@gmail.com>
2023-05-06 22:10:19 -0300

    plan nearly done

--- src/components/house/House.jsx
+++ src/components/house/House.jsx
@@ -19,12 +19,19 @@ function House() {
 			</div>
 
 			<div className="house__text-container">
-				<h2>14 Milena Grove</h2>
-				<h3>House for Sale</h3>
-				<div className="house__ditales-container">
-					<div className="bads"> 2 Bads</div>
-					<div className="baths"> 2 Baths</div>
-					<div className="parking"> 1 Parking</div>
+				<div className="text-box">
+					<h2>14 Milena Grove</h2>
+					<h3>House for Sale</h3>
+					<div className="house__ditales-container">
+						<div className="bads"> 2 Bads</div>
+						<div className="baths"> 2 Baths</div>
+						<div className="parking"> 1 Parking</div>
+					</div>
+				</div>
+			</div>
+			<div className="house__plan-container">
+				<div className="img-box">
+					<img src={home3} alt="" />
 				</div>
 			</div>
 		</div>
